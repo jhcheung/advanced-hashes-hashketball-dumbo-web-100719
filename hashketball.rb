9ad91eba63b71df0e6232ec767ee9a66f895fc4e
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-25 12:14:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,84 @@
 # Write your code here!
 
+require "yaml"
+require "pry"
 
+def game_hash
+  YAML.load_file('./game_hash.yml')
+end 
 
+def num_points_scored(name)
+  game_hash.reduce(nil) do |memo, (key, value)|
+    value[:players].each do |player|
+      if player[name]
+        memo = player[name][:points]
+      end 
+    end
+    memo
+  end 
+end 
 
+def shoe_size(name)
+  game_hash.reduce(nil) do |memo, (key, value)|
+    value[:players].each do |player|
+      if player[name]
+        memo = player[name][:shoe]
+      end 
+    end
+    memo
+  end 
+end 
 
+def team_colors(teamname)
+  game_hash.reduce(nil) do |memo, (key, value)|
+    memo = value[:colors] if teamname == value[:team_name]
+    memo
+  end 
+end 
 
+def team_names
+  game_hash.reduce([]) do |memo, (key, value)|
+    memo.push(value[:team_name])
+    memo
+  end 
+end 
 
+def player_numbers(teamname)
+  game_hash.reduce([]) do |memo, (key, value)|
+    if teamname == value[:team_name]
+      value[:players].each do |player|
+        player.each do |key, value|
+          memo.push(value[:number])
+        end 
+      end 
+    end 
+    memo
+  end 
+end 
 
+def player_stats(name)
+  game_hash.reduce({}) do |memo, (key, value)|
+    value[:players].each do |player|
+      if player[name]
+        memo = player[name]
+      end 
+    end
+    memo
+  end 
+end 
 
-
+def big_shoe_rebounds
+  game_hash.reduce(nil) do |memo, (key, value)|
+    value[:players].each do |player|
+      player.each do |name, stats|
+        maxshoe = stats[:shoe] if !memo
+        memo = stats[:rebounds] if !memo
+        if maxshoe < stats[:shoe]
+          memo = stats[:rebounds] 
+          maxshoe = stats[:shoe]
+        end
+      end 
+    end 
+    memo
+  end 
+end 
